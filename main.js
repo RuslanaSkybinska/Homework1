f6188b4e1f48f85f6929e124c6e8fecbbe33b3b1
RuslanaSkybinska <119412852+RuslanaSkybinska@users.noreply.github.com>
2022-11-30 15:38:33 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+// Task 1 in the main.html file
+
+// Task 2
+
+//alert ("Skybinska")
+
+// Task 3
+
+    //let R = 29;
+    //let S = 657;
+    //console.log (R,S);
+    //R = S;
+    //console.log (R,S);
+
+// Task 4
+
+    /*
+    let company = {
+        name :"SoftServe",
+        years : 25,
+        remote : true,
+        people : undefined,
+        officeOnMoon : null
+    }
+    */
+// Task 5
+
+    /*
+    let isAdult = confirm ("Do you have 18 years old?")
+    console.log (isAdult) 
+    */
+
+// Task 6
+ /*
+    let name = "Ruslana";
+    console.log ("Type of 'name' variable is " + typeof name);
+    let surname = "Skybinska";
+    console.log ("Type of 'surname' variable is " + typeof surname);
+    let group = "Lv-732.TAQC.Retraining";
+    console.log ("Type of 'group' variable is " + typeof group);
+    let yearOfBirth = 1997;
+    console.log ("Type of 'yearOfBirth' variable is " + typeof yearOfBirth);
+    let merried = true;
+    console.log ("Type of 'merried' variable is " + typeof merried);
+
+    console.log (yearOfBirth, merried, name, surname, group)
+
+    let x;
+    let y = null;
+    console.log ("Type of 'x' variable is " + typeof x);
+    console.log ("Type of 'y' variable is " + typeof y);
+    */
+
+// Task 7
+
+   /*
+    let login = prompt ("Enter your login?");
+    let email = prompt ("Enter your email?");
+    let password = prompt ("Enter your password");
+
+    console.log("Dear user your login is " + login + ", your email is " + email + ", your password is " + password); */
+
+// Task 8 
+
+/*
+let secondsInMinute = 60;
+let secondsInHour = secondsInMinute * 60;
+let secondsInDay = secondsInHour * 24;
+let secondsInMonth = secondsInDay * 30;
+
+console.log ("There are " + secondsInHour + " seconds in 1 hour, " + secondsInDay + " seconds in 1 day, " + secondsInMonth + " seconds in months (it is the right number if we take 1 day like 24 hours)");
+*/
